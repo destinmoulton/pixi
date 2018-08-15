4da047991ee860362732fc8ff0590d4184c7cb31
Destin Moulton <destin.moulton@gmail.com>
2018-08-15 15:45:16 -0600

    Created a SorByLowerCaseFileName method/file.

---
+++ dirlist/sort.go
@@ -0,0 +1,25 @@
+package dirlist
+
+import (
+	"os"
+	"strings"
+)
+
+// SortByLowerCaseFilename sorts a slice of file info by lower case
+type SortByLowerCaseFilename []os.FileInfo
+
+func (f SortByLowerCaseFilename) Len() int {
+	return len(f)
+}
+
+func (f SortByLowerCaseFilename) Swap(i, j int) {
+	f[i], f[j] = f[j], f[i]
+}
+
+func (f SortByLowerCaseFilename) Less(i, j int) bool {
+
+	pathA := strings.ToLower(f[i].Name())
+	pathB := strings.ToLower(f[j].Name())
+
+	return pathA < pathB
+}
