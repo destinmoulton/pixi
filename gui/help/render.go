ca734bb0d4e7c8f53dd455d46d17cb1add7dd415
Destin Moulton <destin.moulton@gmail.com>
2018-08-16 14:04:03 -0600

    Added tabs to equalize the help lines.

--- gui/help/render.go
+++ gui/help/render.go
@@ -1,20 +1,26 @@
 package help
 
 import (
+	"bytes"
+	"fmt"
+	"text/tabwriter"
+
 	ui "github.com/gizak/termui"
 )
 
 var helpLines = []string{
-	". or h         Toggle hidden files and folders.",
-	"q or Ctrl+c    Close pixi",
+	".\tToggle hidden files and folders.",
+	"q or Ctrl+c   \tQuit/Exit Pixi",
+	"ESC\tClose Help - Return to Explorer",
 }
 
-var helpList = ui.NewList()
+var helpList = ui.NewPar("")
 
 // Render the help window
 func Render() {
-	helpList.Items = helpLines
-	helpList.BorderLabel = "Help"
+	helpList.Text = tabLines(helpLines)
+	helpList.BorderLabel = "Pixi Help"
+	helpList.Height = ui.TermHeight()
 
 	ui.Clear()
 	ui.Body.Rows = ui.Body.Rows[:0]
@@ -23,4 +29,18 @@ func Render() {
 
 	ui.Body.Align()
 	ui.Render(ui.Body)
+}
+
+func tabLines(lines []string) string {
+	w := new(tabwriter.Writer)
+
+	buf := new(bytes.Buffer)
+	w.Init(buf, 0, 8, 0, ' ', 0)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	fmt.Fprintln(w)
+	w.Flush()
+
+	return buf.String()
 }
