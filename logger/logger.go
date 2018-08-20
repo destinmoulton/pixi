b020faa39d0d700d21e5dace0ad586c792f7a85c
Destin Moulton <destin.moulton@gmail.com>
2018-08-20 16:23:13 -0600

    Created a logger file to setup logging.

---
+++ logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+// Log a pointer to the log Logger
+var Log *log.Logger
+
+// StartLogger initializes logging to file
+func StartLogger() *os.File {
+	cwd, err := os.Getwd()
+	logpath := cwd + "/pixi.log"
+
+	file, err := os.OpenFile(logpath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		panic("Error opening log file")
+	}
+
+	log.SetOutput(file)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	//Log = log.New(f, "", log.LstdFlags|log.Lshortfile)
+	return file
+}
+
+func Println(args ...interface{}) {
+	Log.Println(args...)
+}
