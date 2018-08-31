4c824235e054e0f36203148c56714df95cb2a8e1
Destin Moulton <destin.moulton@gmail.com>
2018-08-31 14:05:23 -0600

    Added events for help.

--- gui/gui.go
+++ gui/gui.go
@@ -5,19 +5,23 @@ import (
 	"github.com/rivo/tview"
 
 	"./explorer"
+	"./help"
 )
 
 var app *tview.Application
+var pages *tview.Pages
 var activePage = "explorer"
 
 // Init starts the gui
 func Init() {
 	app = tview.NewApplication()
-	pages := tview.NewPages()
+	pages = tview.NewPages()
 	redraw := func() {
 		app.Draw()
 	}
 	pages.AddPage("explorer", explorer.UI(redraw), true, true)
+	pages.AddPage("help", help.UI(), true, false)
+
 	explorer.StartExplorer()
 
 	app.SetInputCapture(exitHandler)
@@ -26,13 +30,31 @@ func Init() {
 	}
 }
 
+func switchToPage(page string) {
+	activePage = page
+	pages.SwitchToPage(page)
+}
+
 func exitHandler(eventKey *tcell.EventKey) *tcell.EventKey {
 	if eventKey.Rune() == 'q' {
 		app.Stop()
 		return nil
 	}
 
+	if activePage == "help" {
+		if eventKey.Key() == tcell.KeyEsc || eventKey.Key() == tcell.KeyF1 {
+			switchToPage("explorer")
+			return nil
+		}
+	}
+
 	if activePage == "explorer" {
+
+		if eventKey.Key() == tcell.KeyF1 {
+			switchToPage("help")
+			return nil
+		}
+
 		if eventKey.Key() == tcell.KeyLeft {
 			explorer.NavUpDirectory()
 			return nil
