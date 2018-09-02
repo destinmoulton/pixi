4e42ef197bd2f76a337e07755140329559921a33
Destin Moulton <destin.moulton@gmail.com>
2018-09-02 15:16:20 -0600

    Added add method

--- gui/explorer/history.go
+++ gui/explorer/history.go
@@ -1,8 +1,15 @@
 package explorer
 
+import "path"
+
 type tViewedFile struct {
 	filename string
 	path     string
 }
 
-var history []tViewedFile
+type viewHistory []tViewedFile
+
+func (h *viewHistory) add(fullPath string) {
+	_, filename := path.Split(fullPath)
+	*h = append(*h, tViewedFile{filename, fullPath})
+}
