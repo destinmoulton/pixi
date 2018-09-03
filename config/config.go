e654c3f892484a91b01cc553c83b4d58573e5ff1
Destin Moulton <destin.moulton@gmail.com>
2018-09-03 17:46:20 -0600

    Moved file/json manipulation function to jsonfile.go.

--- config/config.go
+++ config/config.go
@@ -1,9 +1,6 @@
 package config
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 
@@ -63,48 +60,4 @@ func GetInitialDirectory() string {
 	dir, err := os.Getwd()
 	checkErr(err)
 	return dir
-}
-
-func loadAndMapifyConfig() {
-	configFile, err := ioutil.ReadFile(configFullFilePath)
-
-	checkErr(err)
-
-	errj := json.Unmarshal([]byte(configFile), &configMap)
-	checkErr(errj)
-}
-
-func writeConfigFile(jsonMap tConfigMap) {
-	data, err := json.Marshal(&jsonMap)
-	checkErr(err)
-	ioutil.WriteFile(configFullFilePath, data, 0666)
-}
-
-func createConfigFile() {
-	if !doesConfigPathExist() {
-		errD := os.MkdirAll(configDir, os.ModePerm)
-		if errD != nil {
-			panic(fmt.Errorf("Fatal error creating config directory: %s ", errD))
-		}
-	}
-
-	initialJSON := make(tConfigMap)
-
-	initialJSON[KeyLastOpenDirectory] = GetInitialDirectory()
-
-	writeConfigFile(initialJSON)
-}
-
-func doesConfigPathExist() bool {
-	if _, err := os.Stat(configDir); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-func doesConfigFileExist() bool {
-	if _, err := os.Stat(configFullFilePath); os.IsNotExist(err) {
-		return false
-	}
-	return true
 }
--- config/jsonfile.go
+++ config/jsonfile.go
@@ -1,1 +1,52 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func loadAndMapifyConfig() {
+	configFile, err := ioutil.ReadFile(configFullFilePath)
+
+	checkErr(err)
+
+	errj := json.Unmarshal([]byte(configFile), &configMap)
+	checkErr(errj)
+}
+
+func writeConfigFile(jsonMap tConfigMap) {
+	data, err := json.Marshal(&jsonMap)
+	checkErr(err)
+	ioutil.WriteFile(configFullFilePath, data, 0666)
+}
+
+func createConfigFile() {
+	if !doesConfigPathExist() {
+		errD := os.MkdirAll(configDir, os.ModePerm)
+		if errD != nil {
+			panic(fmt.Errorf("Fatal error creating config directory: %s ", errD))
+		}
+	}
+
+	initialJSON := make(tConfigMap)
+
+	initialJSON[KeyLastOpenDirectory] = GetInitialDirectory()
+
+	writeConfigFile(initialJSON)
+}
+
+func doesConfigPathExist() bool {
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func doesConfigFileExist() bool {
+	if _, err := os.Stat(configFullFilePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
