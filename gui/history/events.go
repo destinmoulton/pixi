d257308cd52d830a973befba31a4e16e420241f1
Destin Moulton <destin.moulton@gmail.com>
2018-09-04 16:43:25 -0600

    Created history/events handler.

--- gui/history/events.go
+++ gui/history/events.go
@@ -1,1 +1,11 @@
 package history
+
+import "github.com/gdamore/tcell"
+
+func HandleEvents(eventKey *tcell.EventKey, switchToPage func(string)) *tcell.EventKey {
+	if eventKey.Key() == tcell.KeyEsc || eventKey.Rune() == 'h' {
+		switchToPage("explorer")
+		return eventKey
+	}
+	return eventKey
+}
