3e4c174f9bd9f150dcf758032a7f5dbae31e9355
Destin Moulton <destin.moulton@gmail.com>
2018-09-04 14:52:39 -0600

    Added json conversion to pull apart the interface.

--- gui/explorer/history.go
+++ gui/explorer/history.go
@@ -1,15 +1,38 @@
 package explorer
 
-import "path"
+import (
+	"path"
 
-type tViewedFile struct {
-	filename string
-	path     string
+	"../../settings"
+)
+
+type viewedFile map[string]string
+
+type viewedHistory []viewedFile
+
+var history viewedHistory
+
+func initHistory() {
+	opened := settings.Get(settings.SetHistory, "opened")
+
+	for _, file := range opened.([]interface{}) {
+		// Convert the returned interface (from JSON) into usable map
+		tmp := make(viewedFile)
+		tmp["filename"] = file.(map[string]interface{})["filename"].(string)
+		tmp["path"] = file.(map[string]interface{})["path"].(string)
+		history = append(history, tmp)
+	}
 }
 
-type viewHistory []tViewedFile
-
-func (h *viewHistory) add(fullPath string) {
+func (h *viewedHistory) add(fullPath string) {
 	_, filename := path.Split(fullPath)
-	*h = append(*h, tViewedFile{filename, fullPath})
+
+	file := make(viewedFile)
+	file["filename"] = filename
+	file["path"] = fullPath
+
+	// unshift the new element onto the front of the history
+	*h = append(viewedHistory{file}, *h...)
+
+	settings.Set(settings.SetHistory, "opened", h)
 }
