24b7de34f6b5c511b45432897eb41ff671ec0e25
Destin Moulton <destin.moulton@gmail.com>
2018-10-05 13:33:41 -0600

    Added 's' rune to esc from the settings.

--- gui/settings/events.go
+++ gui/settings/events.go
@@ -4,7 +4,7 @@ import "github.com/gdamore/tcell"
 
 // HandleEvents dispatches key events for the help package
 func HandleEvents(eventKey *tcell.EventKey, switchToPage func(string)) *tcell.EventKey {
-	if eventKey.Key() == tcell.KeyEsc || eventKey.Key() == tcell.KeyF1 {
+	if eventKey.Key() == tcell.KeyEsc || eventKey.Rune() == 's' {
 		switchToPage("explorer")
 		return eventKey
 	}
