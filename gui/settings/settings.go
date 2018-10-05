1a75eff21ba9fc9fc814e0669acaf67dd4d41b6d
Destin Moulton <destin.moulton@gmail.com>
2018-10-05 13:33:50 -0600

    Added basic UI functionality for settings.

---
+++ gui/settings/settings.go
@@ -0,0 +1,22 @@
+package help
+
+import (
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
+
+var uiScreen *tview.Grid
+var uiForm *tview.Form
+
+// UI creates the help window
+func UI() *tview.Grid {
+	uiScreen = tview.NewGrid().SetRows(0).SetColumns(0).SetBorders(true)
+
+	uiFrame := tview.NewFrame(uiForm).
+		AddText("Settings", true, tview.AlignCenter, tcell.ColorDarkMagenta).
+		AddText("ESC or s to leave Settings", false, tview.AlignCenter, tcell.ColorDarkMagenta)
+
+	uiScreen.AddItem(uiFrame, 0, 0, 1, 1, 0, 0, false)
+
+	return uiScreen
+}
