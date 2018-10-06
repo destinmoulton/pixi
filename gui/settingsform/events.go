2c70a5dbbec2ea06ec3ea282327a7cb83c8f978b
Destin Moulton <destin.moulton@gmail.com>
2018-10-06 11:10:12 -0600

    Added local copy of the parent page switch method.

--- gui/settingsform/events.go
+++ gui/settingsform/events.go
@@ -2,8 +2,12 @@ package settingsform
 
 import "github.com/gdamore/tcell"
 
+var parentScreenSwitchPage func(string)
+
 // HandleEvents dispatches key events for the help package
 func HandleEvents(eventKey *tcell.EventKey, switchToPage func(string)) *tcell.EventKey {
+	parentScreenSwitchPage = switchToPage
+
 	if eventKey.Key() == tcell.KeyEsc {
 		switchToPage("explorer")
 		return eventKey
